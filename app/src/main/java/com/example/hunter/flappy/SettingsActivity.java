e06527ebec5fe73907d1fe2be11adf8a70dea038
hbeckmann <hunterbeckmann@gmail.com>
2017-03-08 01:03:44 -0700

    Settings now save

--- app/src/main/java/com/example/hunter/flappy/SettingsActivity.java
+++ app/src/main/java/com/example/hunter/flappy/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.hunter.flappy;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.DisplayMetrics;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 /**
  * Created by Hunter on 3/6/2017.
@@ -26,6 +28,12 @@ public class SettingsActivity extends Activity{
     private LayoutInflater inflater;
     private View itemInflater;
     private LinearLayout settingsMenu;
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
+    private int musicVolume;
+    private int sfxVolume;
+    private SeekBar musicSeekbar;
+    private SeekBar sfxSeekbar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +50,66 @@ public class SettingsActivity extends Activity{
         backdrop = new MainView(context, settingsActivity, viewWidth, viewHeight);
         frame = new FrameLayout(this);
 
+
+        musicSeekbar = (SeekBar) settingsMenu.findViewById(R.id.musicSeekbar);
+        sfxSeekbar = (SeekBar) settingsMenu.findViewById(R.id.sfxSeekbar);
+
+
+
+
+        musicSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            private int progress;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                this.progress = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                editor = sharedPref.edit();
+                editor.clear();
+                editor.putInt("music_volume", progress);
+                editor.commit();
+
+                System.out.println("Current progress on the bar is: " + progress);
+            }
+        });
+
+        sfxSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            private int progress;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                this.progress = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                editor = sharedPref.edit();
+                editor.clear();
+                editor.putInt("sfx_volume", progress);
+                editor.commit();
+
+                System.out.println("Current progress on the bar is: " + progress);
+            }
+        });
+
+        //TODO: warning checkbox
+
+
         frame.addView(backdrop);
         frame.addView(settingsMenu);
         setContentView(frame);
@@ -52,6 +120,9 @@ public class SettingsActivity extends Activity{
     public void onResume() {
         super.onResume();
         backdrop.resume();
+        retrieveSfxVolume();
+        retriveMusicVolume();
+
     }
 
     @Override
@@ -66,5 +137,21 @@ public class SettingsActivity extends Activity{
     public void onStop() {
         super.onStop();
     }
+
+    public void retriveMusicVolume() {
+
+        sharedPref = getSharedPreferences(
+                "Settings", Context.MODE_PRIVATE);
+        musicVolume = sharedPref.getInt("music_volume", 5);
+        musicSeekbar.setProgress(musicVolume);
+    }
+
+    public void retrieveSfxVolume() {
+        sharedPref = getSharedPreferences(
+                "Settings", Context.MODE_PRIVATE);
+        sfxVolume = sharedPref.getInt("sfx_volume", 5);
+        sfxSeekbar.setProgress(sfxVolume);
+    }
+
 
 }
